981241f771d18904060d9e522e13f09e98b9f868
kl <kldevs@proton.me>
2022-05-12 22:24:11 -0400

    Complementary DNA

--- codewars/7kyu/ComplementaryDNA.js
+++ codewars/7kyu/ComplementaryDNA.js
@@ -1,10 +1,6 @@
 // https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/javascript
 
 function DNAStrand(dna) {
-  return dna.split('').map(x => 
-    x === 'A' ? 'T' :
-    x === 'T' ? 'A' :
-    x === 'C' ? 'G' :
-    'C'
-  ).join('')
+  const pairs = {'A':'T','T':'A','C':'G','G':'C'}
+  return dna.split('').map(x => pairs[x]).join('')
 }
