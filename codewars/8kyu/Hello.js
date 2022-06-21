9332d6d3ad19c5d501627df9f88057802ac72a14
kl <kldevs@proton.me>
2022-06-20 23:49:17 -0400

    Hello, Name or World!

---
+++ codewars/8kyu/Hello.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57e3f79c9cb119374600046b
+
+function hello(name){
+  return 'Hello, ' + (name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : 'World') + '!'
+}
