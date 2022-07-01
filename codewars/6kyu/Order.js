c2edb9cac5a46c227ea60f34e80af299940d5741
unknown <kldevs@proton.me>
2022-07-01 17:10:11 -0400

    Your order, please

---
+++ codewars/6kyu/Order.js
@@ -0,0 +1,5 @@
+https://www.codewars.com/kata/55c45be3b2079eccff00010f
+
+function order(words){
+  return words.split(' ').sort((a,b) => a.split('').find(x => !isNaN(x)) - b.split('').find(x => !isNaN(x))).join(' ')      
+}
