400a02f54ee533c0ed2e517be5ad7d9d7df7b6dc
kldevs <kldevs@proton.me>
2022-08-01 16:11:55 -0400

    Maximum subarray sum

--- codewars/5kyu/maxSequence.js
+++ codewars/5kyu/maxSequence.js
@@ -1,14 +1,17 @@
 // https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
 
-var maxSequence = function(arr){
-  if (arr.every(x => x < 0)) return 0
-  else {
-    let currentSum = 0
-    let maxSum = -Infinity
-    for(let i = 0; i < arr.length; i++){ 
-        currentSum = Math.max(arr[i], currentSum + arr[i])
-        maxSum = Math.max(currentSum, maxSum)
+function maxSequence(arr){
+  if(arr.every(x => x < 0)){
+    return 0
+  } else if (arr.every(x => x > 0)){
+    return arr.reduce((s,x) => s + x, 0)
+  }else{
+    sublist = []
+    for(i = 1; i <= arr.length; i++ ){   // length
+      for(j = 0; j <= arr.length - i; j++){  // index
+        sublist.push(arr.slice(j, j + i).reduce((s, x) => s + x, 0)) 
+      }
     }
-    return maxSum
+    return Math.max(...sublist)
   }
 }
